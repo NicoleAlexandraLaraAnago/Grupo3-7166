66de969eba0a630fe990560fe60ccb434f0f6be5
Juan Esteban Gallardo Alava <75323396+Juangallardo-rgb@users.noreply.github.com>
2021-11-28 22:55:01 -0500

    Operaciones Sufijos

--- Parcial 1/Juan Gallardo/Deberes Extra/Extra1/Extra1.cpp
+++ Parcial 1/Juan Gallardo/Deberes Extra/Extra1/Extra1.cpp
@@ -6,27 +6,27 @@ Fecha de modificación: Viernes 29 de Octubre del 2021
 Materia: Estructura de datos
 NRC: 7166*/
 #include <iostream>
-#include<conio.h>
+#include <conio.h>
 
 template <class TIPOD>
-void mostrarAbs(TIPOD numero);
+void mostrarAb(TIPOD numero);
 
 int main() {
 	int num1 = 3;
 	float num2 = 34.79;
 	double num3 = 148.945;
 	
-	mostrarAbs(num1);
-	mostrarAbs(num2);
-	mostrarAbs(num3);
+	mostrarAb(num1);
+	mostrarAb(num2);
+	mostrarAb(num3);
 	getch();
 	return 0;
 }
 
 template <class TIPOD>
-void mostrarAbs(TIPOD numero){
+void mostrarAb(TIPOD numero){
 	if(numero<0){
 		numero = numero * -1;
 	}
-	cout<<"El valor absoluto del numero es: "<<numero<<end1;
+	cout<<"El valor absoluto del numero es: "<<numero<<endl;
 }
